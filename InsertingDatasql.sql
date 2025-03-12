0f8583c6c482e9631a8f0bbd7714dbc04efbb656
Christian Saenz <163576770+ChristianSaenz@users.noreply.github.com>
2025-03-12 14:52:37 -0700

    Update InsertingDatasql.sql

--- InsertingDatasql.sql
+++ InsertingDatasql.sql
@@ -14,7 +14,7 @@ Values
 
 INSERT INTO orders(FirstName, LastName, ToyID, ClothID, Email, OrderNumber, OrderDate)
 Values
-('Christian', 'Saenz', 2, Null, 'Csaenz@linfield.edu', 5, 2025-02-06),
-('Bob', 'sanchez', 3, Null, 'bill@linfield.edu', 8, 2025-02-06),
-('Mike', 'bob', NULL, 3, 'MIke@linfield.edu', 10, 2025-02-06),
-('Billy', 'cruz', 2, 1, 'Blly@linfield.edu', 5, 2025-02-06);
+('Christian', 'Saenz', 2, Null, 'Csaenz@linfield.edu', 5, '2025-02-06'),
+('Bob', 'sanchez', 3, Null, 'bill@linfield.edu', 8, '2025-02-06'),
+('Mike', 'bob', NULL, 3, 'MIke@linfield.edu', 10, '2025-02-06'),
+('Billy', 'cruz', 2, 1, 'Blly@linfield.edu', 5, '2025-02-06');
